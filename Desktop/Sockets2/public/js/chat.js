69d372ee3d7f47dc19f827600bc4aade36cb8457
marianojimenezperez1@gmail.com <marianojimenezperez1@gmail.com>
2021-11-06 13:09:14 -0300

    se agrega autor en el chat

--- Desktop/Sockets2/public/js/chat.js
+++ Desktop/Sockets2/public/js/chat.js
@@ -1,13 +1,15 @@
 const socket = io()
 const chat = document.getElementById('chat');
 const botonEnviar = document.getElementById('boton-enviar');
-botonEnviar.addEventListener('click', () => {
+botonEnviar.addEventListener('click', (e) => {
+    e.preventDefault()
     socket.emit('mensaje', chat.value);
 })
 
 socket.on('mensajes' , data => {
+    let mailCliente = document.getElementById('mail-cliente').value
     const mensajesHTML = data
-        .map(msj => `SocketId: ${msj.socketId} -> Mensaje : ${msj.mensaje}` )
+        .map(msj => `Nombre: ${mailCliente} -> Mensaje : ${msj.mensaje}` )
         .join('<br>')
     const chatHistory = document.getElementById('chat-history');
     chatHistory.innerHTML = mensajesHTML;
